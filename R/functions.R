64fb7be98486a5f375a56c013d2eb64ed84861a1
Signe Kirk Brødbæk <signekb@clin.au.dk>
2023-11-06 13:59:49 +0100

    feat: add descriptive_stats function

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+#' Calculate descriptive stats (mean and standard deviation) of each metabolite
+#'
+#' @param data Lipidomics dataset (loaded from csv)
+#'
+#' @return A data.frame/tibble
+descriptive_stats <- function(data) {
+    data %>%
+        dplyr::group_by(metabolite) %>%
+        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
+        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+}
