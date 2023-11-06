8787f6a4b073b15dcdd4786ba4d27d9f200a50de
Signe Kirk Brødbæk <signekb@clin.au.dk>
2023-11-06 14:31:29 +0100

    feat: update descriptive_stats to include median and iqr

--- R/functions.R
+++ R/functions.R
@@ -4,8 +4,13 @@
 #'
 #' @return A data.frame/tibble
 descriptive_stats <- function(data) {
-    data %>%
-        dplyr::group_by(metabolite) %>%
-        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
-        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+  data %>%
+    dplyr::group_by(metabolite) %>%
+    dplyr::summarise(dplyr::across(value, list(
+      mean = mean,
+      sd = sd,
+      median = median,
+      iqr = IQR
+    ))) %>%
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
