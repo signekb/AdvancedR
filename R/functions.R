eee90471769f0bc39e760a0f0b4b9bb0e6a3e9cb
Signe Kirk Brødbæk <signekb@clin.au.dk>
2023-11-06 14:52:15 +0100

    feat: add plot_distributions()

--- R/functions.R
+++ R/functions.R
@@ -14,3 +14,15 @@ descriptive_stats <- function(data) {
     ))) %>%
     dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+#' Figure showing distributions of metabolites
+#'
+#' @param data Lipidomics data (long format)
+#'
+#' @return A plot object
+
+plot_distributions <- function(data) {
+  ggplot2::ggplot(data, aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(vars(metabolite), scales = "free")
+}
